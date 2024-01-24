51919acb8726862d6195abd20c45270e72a6cda7
gilsonjuniorpro <gilsonjuniorpro@gmail.com>
2024-01-23 20:27:22 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# toggle_flutter
+
+A new Flutter project.
+
+## Getting Started
+
+This project is a starting point for a Flutter application.
+
+A few resources to get you started if this is your first Flutter project:
+
+- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
+
+For help getting started with Flutter development, view the
+[online documentation](https://docs.flutter.dev/), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
+
+![Simulator Screenshot - iPhone 15 Pro - 2024-01-23 at 20.25.54.png](..%2F..%2FDesktop%2FSimulator%20Screenshot%20-%20iPhone%2015%20Pro%20-%202024-01-23%20at%2020.25.54.png)
---
+++ lib/main.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+
+/// Flutter code sample for [SegmentedButton].
+
+void main() {
+  runApp(const SegmentedButtonApp());
+}
+
+class SegmentedButtonApp extends StatelessWidget {
+  const SegmentedButtonApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(useMaterial3: true),
+      home: const Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Spacer(),
+              SingleChoice(),
+              Spacer(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+enum OrdemItem { active, past }
+
+class SingleChoice extends StatefulWidget {
+  const SingleChoice({super.key});
+
+  @override
+  State<SingleChoice> createState() => _SingleChoiceState();
+}
+
+class _SingleChoiceState extends State<SingleChoice> {
+  OrdemItem ordemItem = OrdemItem.active;
+
+  @override
+  Widget build(BuildContext context) {
+    return SegmentedButton<OrdemItem>(
+      segments: const <ButtonSegment<OrdemItem>>[
+        ButtonSegment<OrdemItem>(
+          value: OrdemItem.active,
+          label: Padding(
+            padding: const EdgeInsets.only(left: 16.0, right: 16.0),
+            child: Text('Active orders'),
+          ),
+        ),
+        ButtonSegment<OrdemItem>(
+          value: OrdemItem.past,
+          label: Padding(
+            padding: const EdgeInsets.only(left: 16.0, right: 16.0),
+            child: Text('Past orders'),
+          ),
+        ),
+      ],
+      selected: <OrdemItem>{ordemItem},
+      style: ButtonStyle(
+        // Change background color for selected state
+        backgroundColor: MaterialStateProperty.resolveWith<Color?>((Set<MaterialState> states) {
+          if (states.contains(MaterialState.selected)) {
+            return Colors.blue; // Replace with your desired color
+          }
+          return null; // Use the default color for unselected states
+        }),
+      ),
+      showSelectedIcon: false,
+      onSelectionChanged: (Set<OrdemItem> newSelection) {
+        setState(() {
+          ordemItem = newSelection.first;
+        });
+      },
+    );
+  }
+}
